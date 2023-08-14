b245deb14888bced70380e9a3c5cc452836e2536
koushik173 <cse_2012020173@lus.ac.bd>
2023-08-15 02:57:04 +0600

    crops recoomendation done

--- src/screens/Home/Footer.js
+++ src/screens/Home/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <View className="flex flex-row w-full rounded-tr-full rounded-tl-full h-24 mt-28 bg-orange-200">
-      <Image className="w-60 h-52 rotate-6  mt-2 -top-28 " source={require('../../../Assets/images/ann.png')} />
-      <Image className="w-56 h-44 -left-4 mt-6 -top-28" source={require('../../../Assets/images/savePlant.png')} />
+    <View className="flex flex-row w-full rounded-tr-full rounded-tl-full h-6 opacity-50 mt-10 bg-green-200">
+      {/* <Image className="w-60 h-52 rotate-6  mt-2 -top-28 " source={require('../../../Assets/images/ann.png')} /> */}
+      {/* <Image className="w-56 h-44 -left-4 mt-6 -top-28" source={require('../../../Assets/images/savePlant.png')} /> */}
     </View>
   )
 }
--- src/screens/Home/plantQuest/PlantRecomend.js
+++ src/screens/Home/plantQuest/PlantRecomend.js
@@ -1,17 +1,16 @@
 import { View, Text, ImageBackground, TextInput, Button, Alert, TouchableOpacity } from 'react-native'
 import { useForm, Controller } from "react-hook-form"
 
-import React from 'react'
+import React, { useState } from 'react'
 import GoBack from '../../../components/GoBack'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import axios from 'axios';
+import RecommendPlants from './RecommendPlants';
 
 const PlantRecomend = () => {
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
+  const [cropsData, setCropsData] = useState(false)
+  const [Loading, setLoading] = useState(false);
+  const { control, handleSubmit, reset, formState: { errors } } = useForm({
     defaultValues: {
       Nitrogen: "",
       Phosphorus: "",
@@ -22,168 +21,198 @@ const PlantRecomend = () => {
       Rainfall: ""
     },
   })
-  const onSubmit = (data) =>{
-    console.log(data);
-    reset();
+
+  const onSubmit = async (data) => {
+    setLoading(true)
+    try {
+      const response = await axios.post('http://10.0.2.2:8000/cropsRecommend', data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      // console.log('Response:', response.data);
+      setCropsData(response.data)
+
+    } catch (error) {
+      console.log(error.message);
+      Alert.alert('', error.message, [
+        { text: 'OK', style: 'cancel' },
+      ]);
+    }
+    setLoading(false)
+  }
+
+  const handleClear = () => {
+    setCropsData(false)
+    // reset()
   }
 
   return (
     <SafeAreaView>
       <ImageBackground className="w-full h-full " source={require('../../../../Assets/images/bgTrack.jpg')} resizeMode="cover">
-        <GoBack></GoBack>
-        <Text className="text-black font-bold ml-1 text-white text-xs mt-2">Specific recommendations are given by using nutrient-environment parameters.</Text>
-        <Text className="font-bold text-white text-2xl mt-2 bg-green-700 p-1">Plant Recommendation</Text>
-        
-        
-        <View className="flex flex-wrap h-[350px] gap-2 ml-8 mt-5">
-          <View>
-            <Text className="text-white ml-5 font-bold">Nitrogen</Text>
-            <Controller control={control} rules={{
-              required: {value: true, message:"This is required"}, pattern: {
-                value: /^[0-9]+$/,
-                message: "Enter only number",
-              }
-            }} render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
-                placeholder="Nitrogen"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                type="number"
-              />
-            )}
-              name="Nitrogen"
-            />
-            {errors.Nitrogen && <Text className="text-center text-red-600">{errors.Nitrogen.message}</Text>}
-          </View>
+        {Loading && <Text className="text-lg font-bold text-white">Loading..</Text>}
+        {
+          cropsData ? <RecommendPlants handleClear={handleClear} cropsData={cropsData}></RecommendPlants> :
+            <View>
+              <GoBack></GoBack>
+              <Text className="text-black font-bold ml-1 text-white text-xs mt-2">Specific recommendations are given by using nutrient-environment parameters.</Text>
+              <Text className="font-bold text-white text-2xl mt-2 bg-green-700 p-1">Plant Recommendation</Text>
+              <View className="flex flex-wrap h-[350px] gap-2 ml-8 mt-5">
+                <View>
+                  <Text className="text-white ml-5 font-bold">Nitrogen</Text>
+                  <Controller control={control} rules={{
+                    required: { value: true, message: "This is required" }, pattern: {
+                      value: /^-?\d+(\.\d+)?$/,
+                      message: "Enter only number",
+                    }
+                  }} render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
+                      placeholder="Nitrogen"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                      type="number"
+                    />
+                  )}
+                    name="Nitrogen"
+                  />
+                  {errors.Nitrogen && <Text className="text-center text-red-600">{errors.Nitrogen.message}</Text>}
+                </View>
 
-          <View>
-            <Text className="text-white ml-5 font-bold">Phosphorus </Text>
-            <Controller control={control} rules={{
-              required: {value: true, message:"This is required"}, pattern: {
-                value: /^[0-9]+$/,
-                message: "Enter only number",
-              } }} render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
-                placeholder="Phosphorus"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-              name="Phosphorus"
-            />
-            {errors.Phosphorus && <Text className="text-center text-red-600">{errors.Phosphorus.message}</Text>}
-          </View>
+                <View>
+                  <Text className="text-white ml-5 font-bold">Phosphorus </Text>
+                  <Controller control={control} rules={{
+                    required: { value: true, message: "This is required" }, pattern: {
+                      value: /^-?\d+(\.\d+)?$/,
+                      message: "Enter only number",
+                    }
+                  }} render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
+                      placeholder="Phosphorus"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                    name="Phosphorus"
+                  />
+                  {errors.Phosphorus && <Text className="text-center text-red-600">{errors.Phosphorus.message}</Text>}
+                </View>
 
-          <View>
-            <Text className="text-white ml-5 font-bold">Potassium</Text>
-            <Controller control={control} rules={{
-              required: {value: true, message:"This is required"}, pattern: {
-                value: /^[0-9]+$/,
-                message: "Enter only number",
-              } }} render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
-                placeholder="Potassium"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-              name="Potassium"
-            />
-            {errors.Potassium && <Text className="text-center text-red-600">{errors.Potassium.message}</Text>}
-          </View>
+                <View>
+                  <Text className="text-white ml-5 font-bold">Potassium</Text>
+                  <Controller control={control} rules={{
+                    required: { value: true, message: "This is required" }, pattern: {
+                      value: /^-?\d+(\.\d+)?$/,
+                      message: "Enter only number",
+                    }
+                  }} render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
+                      placeholder="Potassium"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                    name="Potassium"
+                  />
+                  {errors.Potassium && <Text className="text-center text-red-600">{errors.Potassium.message}</Text>}
+                </View>
 
-          <View>
-            <Text className="text-white ml-5 font-bold">Temperature</Text>
-            <Controller control={control} rules={{
-              required: {value: true, message:"This is required"}, pattern: {
-                value: /^[0-9]+$/,
-                message: "Enter only number",
-              } }} render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
-                placeholder="Temperature"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-              name="Temperature"
-            />
-            {errors.Temperature && <Text className="text-center text-red-600">{errors.Temperature.message}</Text>}
-          </View>
+                <View>
+                  <Text className="text-white ml-5 font-bold">Temperature</Text>
+                  <Controller control={control} rules={{
+                    required: { value: true, message: "This is required" }, pattern: {
+                      value: /^-?\d+(\.\d+)?$/,
+                      message: "Enter only number",
+                    }
+                  }} render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
+                      placeholder="Temperature"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                    name="Temperature"
+                  />
+                  {errors.Temperature && <Text className="text-center text-red-600">{errors.Temperature.message}</Text>}
+                </View>
 
-          <View>
-            <Text className="text-white ml-5 font-bold">Humidity</Text>
-            <Controller control={control} rules={{
-              required: {value: true, message:"This is required"}, pattern: {
-                value: /^[0-9]+$/,
-                message: "Enter only number",
-              } }} render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
-                placeholder="Humidity"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-              name="Humidity"
-            />
-            {errors.Humidity && <Text className="text-center text-red-600">{errors.Humidity.message}</Text>}
-          </View>
+                <View>
+                  <Text className="text-white ml-5 font-bold">Humidity</Text>
+                  <Controller control={control} rules={{
+                    required: { value: true, message: "This is required" }, pattern: {
+                      value: /^-?\d+(\.\d+)?$/,
+                      message: "Enter only number",
+                    }
+                  }} render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
+                      placeholder="Humidity"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                    name="Humidity"
+                  />
+                  {errors.Humidity && <Text className="text-center text-red-600">{errors.Humidity.message}</Text>}
+                </View>
 
-          <View>
-            <Text className="text-white ml-5 font-bold">Ph</Text>
-            <Controller control={control} rules={{
-              required: {value: true, message:"This is required"}, pattern: {
-                value: /^[0-9]+$/,
-                message: "Enter only number",
-              } }} render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
-                placeholder="Ph"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-              name="Ph"
-            />
-            {errors.Ph && <Text className="text-center text-red-600">{errors.Ph.message}</Text>}
-          </View>
+                <View>
+                  <Text className="text-white ml-5 font-bold">PH</Text>
+                  <Controller control={control} rules={{
+                    required: { value: true, message: "This is required" }, pattern: {
+                      value: /^-?\d+(\.\d+)?$/,
+                      message: "Enter only number",
+                    }
+                  }} render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
+                      placeholder="PH"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                    name="Ph"
+                  />
+                  {errors.Ph && <Text className="text-center text-red-600">{errors.Ph.message}</Text>}
+                </View>
 
-          <View>
-            <Text className="text-white ml-5 font-bold">Rainfall</Text>
-            <Controller control={control} rules={{
-              required: {value: true, message:"This is required"}, pattern: {
-                value: /^[0-9]+$/,
-                message: "Enter only number",
-              } }} render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
-                placeholder="Rainfall"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-              name="Rainfall"
-            />
-            {errors.Rainfall && <Text className="text-center text-red-600">{errors.Rainfall.message}</Text>}
-          </View>
+                <View>
+                  <Text className="text-white ml-5 font-bold">Rainfall</Text>
+                  <Controller control={control} rules={{
+                    required: { value: true, message: "This is required" }, pattern: {
+                      value: /^-?\d+(\.\d+)?$/,
+                      message: "Enter only number",
+                    }
+                  }} render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      className="bg-white rounded-full w-36 opacity-60 text-black text-center font-bold text-lg"
+                      placeholder="Rainfall"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                    name="Rainfall"
+                  />
+                  {errors.Rainfall && <Text className="text-center text-red-600">{errors.Rainfall.message}</Text>}
+                </View>
 
-        </View>
-        <TouchableOpacity onPress={handleSubmit(onSubmit)} className="bg-white rounded-full w-60 ml-16 opacity-70 p-2">
-          <Text className="font-bold text-lg text-black text-center"> Recommend</Text>
-        </TouchableOpacity>
+              </View>
 
-
+              <TouchableOpacity onPress={handleSubmit(onSubmit)} className="bg-white rounded-full w-60 ml-16 opacity-70 p-2">
+                <Text className="font-bold text-lg text-black text-center">Recommend</Text>
+              </TouchableOpacity>
+            </View>
+        }
 
       </ImageBackground>
     </SafeAreaView>
